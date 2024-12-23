75aa581f7474d4eb992059fded1ce0f9309782fc
haissambhaur <sambhaur@gmail.com>
2024-12-23 22:39:02 +0500

    first commit after change in the new branch to test out the feature branch

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def hello_name():
-   return 'Hello World' 
+@app.route('/hello/<name>')
+def hello_name(name):
+   return 'Hello %s!' % name
 if __name__ == '__main__':
    app.run()
